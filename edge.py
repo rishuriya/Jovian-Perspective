9384819498dbd7ce32bb2419f885fa83d2090b1d
rishuriya <rdrishav2002@yahoo.com>
2022-09-30 17:43:32 +0530

    added UI

---
+++ edge.py
@@ -0,0 +1,106 @@
+
+from PyQt6 import QtCore, QtGui, QtWidgets
+
+
+class Ui_edge(object):
+    def setupUi(self, Form,File):
+        Form.setObjectName("Form")
+        Form.resize(889, 612)
+        Form.setStyleSheet("*{border:none;\n"
+"background-color:#fff;\n"
+"color:#000;}\n"
+"#ui{\n"
+"background-color:rgb(97, 53, 131)\n"
+"}\n"
+"QPushButton{\n"
+"background-color:#071e26;\n"
+"color:#fff}")
+        self.verticalLayout = QtWidgets.QVBoxLayout(Form)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.frame = QtWidgets.QFrame(Form)
+        self.frame.setMinimumSize(QtCore.QSize(0, 50))
+        self.frame.setMaximumSize(QtCore.QSize(16777215, 50))
+        self.frame.setFrameShape(QtWidgets.QFrame.Shape.StyledPanel)
+        self.frame.setFrameShadow(QtWidgets.QFrame.Shadow.Raised)
+        self.frame.setObjectName("frame")
+        self.save = QtWidgets.QPushButton(self.frame)
+        self.save.setGeometry(QtCore.QRect(738, 4, 121, 41))
+        icon = QtGui.QIcon.fromTheme("document-save")
+        self.save.setIcon(icon)
+        self.save.setObjectName("save")
+        self.discard = QtWidgets.QPushButton(self.frame)
+        self.discard.setGeometry(QtCore.QRect(598, 4, 121, 41))
+        icon = QtGui.QIcon.fromTheme("edit-clear")
+        self.discard.setIcon(icon)
+        self.discard.setObjectName("discard")
+        self.heading = QtWidgets.QLabel(self.frame)
+        self.heading.setGeometry(QtCore.QRect(6, 6, 371, 41))
+        font = QtGui.QFont()
+        font.setFamily("Ubuntu Condensed")
+        font.setPointSize(24)
+        self.heading.setFont(font)
+        self.heading.setAlignment(QtCore.Qt.AlignmentFlag.AlignCenter)
+        self.heading.setObjectName("heading")
+        self.heading.setText("Edge Detection")
+        self.verticalLayout.addWidget(self.frame, 0, QtCore.Qt.AlignmentFlag.AlignTop)
+        self.frame_2 = QtWidgets.QFrame(Form)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Policy.Expanding, QtWidgets.QSizePolicy.Policy.Expanding)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.frame_2.sizePolicy().hasHeightForWidth())
+        self.frame_2.setSizePolicy(sizePolicy)
+        self.frame_2.setFrameShape(QtWidgets.QFrame.Shape.StyledPanel)
+        self.frame_2.setFrameShadow(QtWidgets.QFrame.Shadow.Raised)
+        self.frame_2.setObjectName("frame_2")
+        self.label = QtWidgets.QLabel(self.frame_2)
+        self.label.setGeometry(QtCore.QRect(70, 20, 451, 491))
+        self.label.setText("")
+        self.label.setObjectName("label")
+        global fname
+        fname=File
+        pixmap = QtGui.QPixmap(fname)
+        print(pixmap)
+        self.label.setPixmap(pixmap)
+        self.label.setScaledContents(True)
+        self.ui = QtWidgets.QFrame(self.frame_2)
+        self.ui.setGeometry(QtCore.QRect(590, 0, 281, 541))
+        self.ui.setFrameShape(QtWidgets.QFrame.Shape.StyledPanel)
+        self.ui.setFrameShadow(QtWidgets.QFrame.Shadow.Raised)
+        self.ui.setObjectName("ui")
+        self.verticalSlider = QtWidgets.QSlider(self.ui)
+        self.verticalSlider.setGeometry(QtCore.QRect(130, 50, 20, 231))
+        self.verticalSlider.setOrientation(QtCore.Qt.Orientation.Vertical)
+        self.verticalSlider.setObjectName("verticalSlider")
+        self.compare = QtWidgets.QPushButton(self.ui)
+        self.compare.setGeometry(QtCore.QRect(80, 310, 131, 41))
+        icon = QtGui.QIcon.fromTheme("system-reboot")
+        self.compare.setIcon(icon)
+        self.compare.setObjectName("compare")
+        self.reset = QtWidgets.QPushButton(self.ui)
+        self.reset.setGeometry(QtCore.QRect(78, 384, 131, 41))
+        icon = QtGui.QIcon.fromTheme("document-open")
+        self.reset.setIcon(icon)
+        self.reset.setObjectName("reset")
+        self.verticalLayout.addWidget(self.frame_2)
+
+        self.retranslateUi(Form)
+        QtCore.QMetaObject.connectSlotsByName(Form)
+
+    def retranslateUi(self, Form):
+        _translate = QtCore.QCoreApplication.translate
+        Form.setWindowTitle(_translate("Form", "Form"))
+        self.save.setText(_translate("Form", "Save"))
+        self.discard.setText(_translate("Form", "Discard"))
+        self.heading.setText(_translate("Form", "Edge Detection"))
+        self.compare.setText(_translate("Form", "Reset"))
+        self.reset.setText(_translate("Form", "Compare"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    Form = QtWidgets.QWidget()
+    ui = Ui_edge()
+    ui.setupUi(Form)
+    Form.show()
+    sys.exit(app.exec())
