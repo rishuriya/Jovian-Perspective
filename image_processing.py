4d5b3be5767e76e207f31198254ae3b1348e6f74
Harikrishna-AL <amenu4aie20134@am.students.amrita.edu>
2022-10-02 17:06:07 +0530

    colors added

--- image_processing.py
+++ image_processing.py
@@ -10,7 +10,8 @@ def combineRGB(r,g,b):
   return out
 
 # to get different variations of R, G and B
-def RGBvariation(r,g,b,rvar=1,gvar=1,bvar=1):
+def RGBvariation(img,rvar=1,gvar=1,bvar=1):
+  b,g,r = cv2.split(img)
   r = r*rvar
   
   r = r.astype('uint8')
@@ -20,7 +21,7 @@ def RGBvariation(r,g,b,rvar=1,gvar=1,bvar=1):
   b = b*bvar
   
   b = b.astype('uint8')
-  out = cv2.merge((r,g,b))
+  out = cv2.merge((b,g,r))
   return out
 
 # To increse the brighness
@@ -118,3 +119,33 @@ def gauss_diff(image, kernel_size=5):
     img_unsharp=img+(img-gaussian)
     # diff = cv2.absdiff(gray, gauss)
     return img_unsharp
+
+def equalize_this(image_file, with_plot=False, gray_scale=False):
+    image_src = read_this(image_file=image_file, gray_scale=gray_scale)
+    if not gray_scale:
+        r_image, g_image, b_image = cv2.split(image_src)
+
+        r_image_eq = cv2.equalizeHist(r_image)
+        g_image_eq = cv2.equalizeHist(g_image)
+        b_image_eq = cv2.equalizeHist(b_image)
+
+        image_eq = cv2.merge((r_image_eq, g_image_eq, b_image_eq))
+        cmap_val = None
+    else:
+        image_eq = cv2.equalizeHist(image_src)
+        cmap_val = 'gray'
+
+    # if with_plot:
+    #     fig = plt.figure(figsize=(10, 20))
+
+    #     ax1 = fig.add_subplot(2, 2, 1)
+    #     ax1.axis("off")
+    #     ax1.title.set_text('Original')
+    #     ax2 = fig.add_subplot(2, 2, 2)
+    #     ax2.axis("off")
+    #     ax2.title.set_text("Equalized")
+
+    #     ax1.imshow(image_src, cmap=cmap_val)
+    #     ax2.imshow(image_eq, cmap=cmap_val)
+    #     return True
+    return image_eq
